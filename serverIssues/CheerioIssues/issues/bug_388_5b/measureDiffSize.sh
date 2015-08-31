79051d0f97013153d3f7cf688566cc48dee260a6
marija <m.selakovic89@gmail.com>
2015-08-31 11:24:01 +0200

    more polishing

--- serverIssues/CheerioIssues/issues/bug_388_5b/measureDiffSize.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-printf "${PWD##*/}: "
-../../measureDiffSize.sh cheerio_before/lib/render.js cheerio_after/lib/render.js
--- serverIssues/CheerioIssues/issues/issue_386b/measureDiffSize.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-printf "${PWD##*/}: "
-../../measureDiffSize.sh cheerio_before/lib/api/attributes.js cheerio_after/lib/api/attributes.js
